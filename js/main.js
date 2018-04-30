41c3c612e6fd5d8d101397e80e0f19a433492969
Valentin Burkin <burkinman@me.com>
2018-04-30 17:11:19 +0300

    style: Fix view on the main page

--- js/main.js
+++ js/main.js
@@ -143,22 +143,27 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const info = document.createElement('div');
+  info.className = 'restaurant-info';
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  info.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  info.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  info.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  info.append(more)
+
+  li.append(info);
 
   return li
 }
